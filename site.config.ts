0e4c0410685a64c4270ac35d03d562a160a42b99
花花 <73741466+xLikeWATCHDOG@users.noreply.github.com>
2024-05-12 20:59:06 +0800

    reset notion page id

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '0b419230781145f99ccd1ed1cf1a7c67',
+  rootNotionPageId: 'a8c3862554094c1fbd36cb22a3a04264',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
